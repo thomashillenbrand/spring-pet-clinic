5e559f60747a34f6db662db10c0b31f73328d0a0
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-11-05 21:32:54 -0600

    Added base entity with ID. Closes #10

--- pet-clinic-data/src/main/java/learn/spring/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/learn/spring/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package learn.spring.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstname;
     private String lastname;
--- pet-clinic-data/src/main/java/learn/spring/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/learn/spring/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package learn.spring.springpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
