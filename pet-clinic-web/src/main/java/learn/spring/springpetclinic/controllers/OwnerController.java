116657b5662a38c4c624829a3b2133fdba92b699
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-11-06 17:10:53 -0600

    Added owner index page and controller, closes #13

---
+++ pet-clinic-web/src/main/java/learn/spring/springpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package learn.spring.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String ownerIndex() {
+        return "/owners/index";
+    }
+
+}
