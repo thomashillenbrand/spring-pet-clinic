c5fa71b3c6337215fc9cbb6aabccc95e266b36bb
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-11-09 14:27:02 -0600

    Listed vets on index page, closes #15

--- pet-clinic-web/src/main/java/learn/spring/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/learn/spring/springpetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package learn.spring.springpetclinic.controllers;
 
+import learn.spring.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/", "/vets/index.html"})
-    public String vetIndex() {
+    public String vetIndex(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
