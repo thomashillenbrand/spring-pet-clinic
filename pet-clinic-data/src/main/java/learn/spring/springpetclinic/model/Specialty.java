3f2c27ccb47f40dedfb66161d69e0de72a9151e7
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-11-13 15:41:48 -0600

    Added vet sepcialities, and added to vets. closes #27

---
+++ pet-clinic-data/src/main/java/learn/spring/springpetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package learn.spring.springpetclinic.model;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
