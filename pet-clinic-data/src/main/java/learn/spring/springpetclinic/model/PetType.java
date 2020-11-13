ee2cd0329d3bb2597a1241e751c7ebe21bdaffdb
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-11-13 14:52:32 -0600

    Added visits, updated owner with pet realtionship. Closes #26

--- pet-clinic-data/src/main/java/learn/spring/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/learn/spring/springpetclinic/model/PetType.java
@@ -4,4 +4,11 @@ public class PetType extends BaseEntity {
 
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
