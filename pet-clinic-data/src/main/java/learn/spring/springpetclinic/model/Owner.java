4abde25209d8fe9576916997c9586f5704af03da
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-11-16 17:33:02 -0600

    Convery owner to JPA entity, closes #36

--- pet-clinic-data/src/main/java/learn/spring/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/learn/spring/springpetclinic/model/Owner.java
@@ -1,13 +1,24 @@
 package learn.spring.springpetclinic.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner") // mappe by owner value on a pet
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/learn/spring/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/learn/spring/springpetclinic/model/Person.java
@@ -1,8 +1,14 @@
 package learn.spring.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name") // thsi is the default column name anyway
     private String firstname;
+    @Column(name = "last_name")
     private String lastname;
 
     public String getFirstname() {
