42709deb23d20bac74d924ff65cbd5666660f0b2
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-12-06 17:43:18 -0600

    Added Display Owner page

--- pet-clinic-web/src/main/java/learn/spring/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/learn/spring/springpetclinic/controllers/OwnerController.java
@@ -1,9 +1,13 @@
 package learn.spring.springpetclinic.controllers;
 
+import learn.spring.springpetclinic.model.Owner;
 import learn.spring.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -24,6 +28,15 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwner() {
         return "NotImplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        Owner owner = ownerService.findById(ownerId);
+
+        mav.addObject(owner);
+        return mav;
     }
 
 }
