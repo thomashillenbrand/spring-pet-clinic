3753ce799c74891d9206f633300b3b6b185978a4
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-12-08 17:49:14 -0600

    Added model attrbutes for Pet Controller. #56

---
+++ pet-clinic-web/src/main/java/learn/spring/springpetclinic/controllers/PetController.java
@@ -0,0 +1,50 @@
+package learn.spring.springpetclinic.controllers;
+
+import learn.spring.springpetclinic.model.Owner;
+import learn.spring.springpetclinic.model.PetType;
+import learn.spring.springpetclinic.services.OwnerService;
+import learn.spring.springpetclinic.services.PetService;
+import learn.spring.springpetclinic.services.PetTypeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEW_PET_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final PetService petService;
+    private final PetTypeService petTypeService;
+    private final OwnerService ownerService;
+
+    public PetController(PetService petService, PetTypeService petTypeService, OwnerService ownerService) {
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+        this.ownerService = ownerService;
+    }
+
+    @ModelAttribute("types") // automatically adds return value to the model
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+}
